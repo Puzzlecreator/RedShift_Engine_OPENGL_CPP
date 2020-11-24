b6991d56aeddeeaddd54713096d264fa2388b1a1
Puzzlecreator <48571170+Puzzlecreator@users.noreply.github.com>
2020-11-24 10:25:13 -0500

    Code base Update 1.2

---
+++ RedShift_Engine/com/Lib/Definitions/VectorDefs.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#define VECTOR_INT std::vector<int>
+#define VECTOR_FLOAT std::vector<float>
+#define VECTOR_DOUBLE std::vector<double>
+#define VECTOR_BOOLEAN std::vector<bool>
+#define VECTOR_STRING std::vector<std::string>
+#define VECTOR_CHAR std::vector<char>
+#define VECTOR_LONG std::vector<long>
+#define VECTOR_VOID std::vector<void>
+#define VECTOR_SHORT std::vector<short>
